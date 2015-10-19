edb08b49b261e128fb35049fd5f407a261c5957a
michaelpitz <michael.pitz@cf2m.be>
2015-10-19 16:27:30 +0200

    détail d'article

--- src/Perso/ArticleBundle/Controller/DefaultController.php
+++ src/Perso/ArticleBundle/Controller/DefaultController.php
@@ -9,7 +9,30 @@ class DefaultController extends Controller
     // pas de variables get pour l'accueil
     public function indexAction()
     {
-        // affichage de la vue twig sans variables
-        return $this->render('PersoArticleBundle:Default:index.html.twig');
+        // création du conteneur de l'objet gérant (via doctrine) la classe Article
+        $repository = $this->getDoctrine()
+                ->getRepository('PersoArticleBundle:Article');
+        
+        // on récupère tous les articles (équivalent select * from article)
+        $prend = $repository->findAll();
+        
+        // affichage de la vue twig avec la variable affiche (twig) et $prend (résultat de la requête)
+        return $this->render('PersoArticleBundle:Default:index.html.twig',array("affiche"=>$prend));
+        
+    }
+    
+    // Page de détail d'un article
+    public function afficheAction($id)
+    {
+        // création du conteneur de l'objet gérant (via doctrine) la classe Article
+        $repository = $this->getDoctrine()
+                ->getRepository('PersoArticleBundle:Article');
+        
+        // on récupère tous les articles (équivalent select * from article WHERE id = $id)
+        $prend = $repository->find($id);
+        
+        // affichage de la vue twig avec la variable affiche (twig) et $prend (résultat de la requête)
+        return $this->render('PersoArticleBundle:Default:affiche.html.twig',array("affiche"=>$prend));
+        
     }
 }
